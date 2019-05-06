507be284f779055f9366f2608129cad60ac4e3a1
Ming Liu <mliu@macpro.home>
2019-05-06 17:49:23 -0400

    stretch

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import Movie from './Movie';
 import MovieCard from './MovieCard';
 
 
