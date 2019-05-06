6eeee0c2685c4cf2f486a090b088dd5d2a0bf9c0
Ming Liu <mliu@macpro.home>
2019-05-06 16:07:07 -0400

    setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Route>
+        <App />
+    </Route>, 
+document.getElementById('root')
+);
