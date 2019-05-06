1e91218236a52ea5dce99475601d8b24ae80d46a
Ming Liu <mliu@macpro.home>
2019-05-06 17:17:29 -0400

    movie mvp and 1 strech on refactoring moviecard

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <Route>
+    <Router>
         <App />
-    </Route>, 
+    </Router>, 
 document.getElementById('root')
 );
